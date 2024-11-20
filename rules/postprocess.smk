4f7c5706e6526856d9cddaf4050548e87e36e97c
LukasFrankenQ <lukas.franken@ed.ac.uk>
2024-11-20 22:46:28 +0000

    complete model summary pipeline

--- rules/postprocess.smk
+++ rules/postprocess.smk
@@ -8,9 +8,9 @@ rule summarize_system_cost:
         offers="data/base/{day}/offers.csv",
         cfd_strike_prices="data/preprocessed/cfd_strike_prices.csv",
         roc_values="data/preprocessed/roc_values.csv",
-        network_nodal="results/networks/{day}/network_s_nodal_solved.nc",
-        network_national="results/networks/{day}/network_s_national_solved.nc",
-        network_national_redispatch="results/networks/{day}/network_s_national_solved_redispatch.nc",
+        network_nodal="results/{day}/network_s_nodal_solved.nc",
+        network_national="results/{day}/network_s_national_solved.nc",
+        network_national_redispatch="results/{day}/network_s_national_solved_redispatch.nc",
     output:
         system_cost_summary="results/{day}/system_cost_summary.csv",
     resources:
--- scripts/summarize_system_cost.py
+++ scripts/summarize_system_cost.py
@@ -37,7 +37,7 @@ def get_wholesale_cost(n):
 
 def get_congestion_rents(n):
 
-    congestion_rent = 0
+    congestion_rent = pd.Series(0, index=n.snapshots)
 
     for branch, getter in zip(['lines', 'links'], ['s', 'p']):
 
@@ -56,7 +56,7 @@ def get_congestion_rents(n):
 
             congestion_rent += (prices1 - prices0).abs() * flow.abs() * 0.5
     
-    return congestion_rent
+    return congestion_rent.mul(-1)
 
 
 def get_roc_cost(bal, roc):
@@ -66,7 +66,8 @@ def get_roc_cost(bal, roc):
     therefore bal refers to the network after redispatch (in a
     nodal market insert the nodal wholesale market).
     '''
-    roc_payments = 0
+
+    roc_payments = pd.Series(0, index=bal.snapshots)
 
     for plant, roc_price in roc.items():
 
@@ -79,7 +80,7 @@ def get_roc_cost(bal, roc):
         else:
             print(f'Plant {plant} not found in network')
             continue
-            
+
         roc_payments += roc_price * activity * 0.5
 
     return roc_payments
@@ -92,11 +93,11 @@ def get_cfd_payments(n, strike_prices):
 
     strike_prices = (
         strike_prices
-        .loc[:,:n.snapshots[0]]
+        .loc[:,:n.snapshots[4]]
         .iloc[:,-1]
     )
 
-    cfd_payments = 0
+    cfd_payments = pd.Series(0, index=n.snapshots)
     for plant, strike_price in strike_prices.items():
 
         price_gap = (
@@ -105,6 +106,112 @@ def get_cfd_payments(n, strike_prices):
         cfd_payments += n.generators_t.p[plant] * price_gap * 0.5
 
     return cfd_payments
+
+
+def get_bidding_volume(nat, bal):
+    """
+    Returns the bidding volume of the network.
+
+    Parameters:
+        nat(pypsa.Network): The national wholesale market optimum.
+        bal(pypsa.Network): The wholesale market after redispatch.
+    """
+
+    lat_bidding_threshold = 55.3 # groups dispatchable generators into north and south
+
+    wind = nat.generators.index[nat.generators.carrier.str.contains('wind')]
+    water = nat.storage_units.index[nat.storage_units.carrier.isin(['cascade', 'hydro'])]
+    solar = nat.generators.index[nat.generators.carrier.str.contains('solar')]
+
+    gen = bal.generators
+    gen['y'] = gen.bus.map(bal.buses.y)
+
+    dispatchable_south = gen.loc[
+        (gen.carrier.isin(['fossil', 'biomass', 'coal'])) &
+        (gen['y'] <= lat_bidding_threshold)
+    ].index
+    dispatchable_north = gen.loc[
+        (gen.carrier.isin(['fossil', 'biomass', 'coal'])) &
+        (gen['y'] > lat_bidding_threshold)
+    ].index
+
+    bidding_volume = pd.Series(0, nat.snapshots)
+
+    for plant in [wind, solar, dispatchable_south, dispatchable_north]:
+        
+        bidding_volume += (
+            nat.generators_t.p[plant].sum(axis=1) -
+            bal.generators_t.p[plant].sum(axis=1)
+        ).clip(lower=0)
+    
+    bidding_volume += (
+        nat.storage_units_t.p[water].sum(axis=1) -
+        bal.storage_units_t.p[water].sum(axis=1)
+    ).clip(lower=0)
+    
+    return bidding_volume
+
+
+def get_balancing_cost(
+        wholesale,
+        balanced,
+        actual_bids,
+        actual_offers
+        ):
+
+    bidding_volume = get_bidding_volume(wholesale, balanced)
+    bidding_volume.index = pd.to_datetime(bidding_volume.index, utc=True)
+
+    bid_costs = pd.Series(0, index=bidding_volume.index)
+    offer_costs = pd.Series(0, index=bidding_volume.index)
+
+    bid_default_cost = 50 # £/MWh
+    offer_default_cost = 90 # £/MWh
+
+    for dt, model_vol in bidding_volume.items():
+
+        model_vol *= 0.5  # MW -> MWh
+
+        period_bids = actual_bids.loc[dt].T.sort_values(by='price').dropna()
+        period_bids['cumvol'] = period_bids['vol'].cumsum()
+
+        period_offers = actual_offers.loc[dt].T.sort_values(by='price').dropna()
+        period_offers['cumvol'] = period_offers['vol'].cumsum()
+
+        if period_bids.empty:
+            bid_costs.loc[dt] += bid_default_cost * model_vol
+
+        elif model_vol > period_bids['vol'].sum():
+            bid_costs.loc[dt] += period_bids['price'].dot(period_bids['vol'])
+            bid_costs.loc[dt] += period_bids['price'].iloc[-1] * (model_vol - period_bids['vol'].sum())
+
+        else:
+            ss = period_bids.loc[period_bids['cumvol'] < model_vol]
+            rest = period_bids.loc[period_bids['cumvol'] >= model_vol]
+
+            bid_costs.loc[dt] += ss['price'].dot(ss['vol'])
+            bid_costs.loc[dt] += (model_vol - ss['vol'].sum()) * rest['price'].iloc[0]
+        
+        if period_offers.empty:
+            offer_costs.loc[dt] += offer_default_cost * model_vol
+
+        elif model_vol > period_offers['vol'].sum():
+            offer_costs.loc[dt] += (
+                period_offers['price'].dot(period_offers['vol'])
+            )
+            offer_costs.loc[dt] += (
+                period_offers['price'].iloc[-1] * (model_vol - period_offers['vol'].sum())
+            )
+        
+        else:
+            ss = period_offers.loc[period_offers['cumvol'] < model_vol]
+            rest = period_offers.loc[period_offers['cumvol'] >= model_vol]
+
+            offer_costs.loc[dt] += ss['price'].dot(ss['vol'])
+            offer_costs.loc[dt] += (model_vol - ss['vol'].sum()) * rest['price'].iloc[0]
+
+    return bid_costs, offer_costs
+
 
 
 if __name__ == '__main__':
@@ -116,14 +223,51 @@ if __name__ == '__main__':
 
     nat_bal = pypsa.Network(
         snakemake.input.network_national_redispatch
-        ) # national system post balancing
+        ) # national system after redispatch
     
     logger.info('Computing Balancing Costs')
 
-    bids = pd.read_csv(snakemake.input.bids)
-    offers = pd.read_csv(snakemake.input.offers)
+    bids = pd.read_csv(snakemake.input.bids, index_col=[0,1], parse_dates=True)
+    offers = pd.read_csv(snakemake.input.offers, index_col=[0,1], parse_dates=True)
 
+    cfd_strike_prices = pd.read_csv(
+        snakemake.input.cfd_strike_prices,
+        index_col=0,
+        parse_dates=True
+        )
 
+    roc_values = pd.read_csv(snakemake.input.roc_values, index_col=0).iloc[:,0]
 
+    congestion_rent_compensation_share = 0.5
+    # share of congestion rents that are not interpreted
+    # and instead are assumed to be paid to the generators
 
-    logger.info('Computing Congestion Rents')
+    bidcosts, offercosts = get_balancing_cost(nat, nat_bal, bids, offers)
+    total_national_costs = pd.DataFrame(
+        {
+            'wholesale': get_wholesale_cost(nat).values,
+            'congestion_rent': get_congestion_rents(nat).values * congestion_rent_compensation_share,
+            'cfd_payments': get_cfd_payments(nat, cfd_strike_prices).values,
+            'roc_payments': get_roc_cost(nat_bal, roc_values).values,
+            'offer_cost': offercosts.values,
+            'bid_cost': bidcosts.values,
+        },
+        index=nat.snapshots
+    ).mul(1e-6)
+
+    total_nodal_costs = pd.DataFrame(
+        {
+            'wholesale': get_wholesale_cost(nod).values,
+            'congestion_rent': get_congestion_rents(nod).values * congestion_rent_compensation_share,
+            'cfd_payments': get_cfd_payments(nod, cfd_strike_prices).values,
+            'roc_payments': get_roc_cost(nod, roc_values).values,
+            'offer_cost': 0,
+            'bid_cost': 0,
+        },
+        index=nat.snapshots
+    ).mul(1e-6)
+
+    pd.concat((
+        total_nodal_costs.stack().rename('nodal'),
+        total_national_costs.stack().rename('national')
+    ), axis=1).to_csv(snakemake.output.system_cost_summary)
