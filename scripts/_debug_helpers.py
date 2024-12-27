2d1cccc52e729a6cd301e3f442f80581584c6be8
LukasFrankenQ <lukas.franken@ed.ac.uk>
2024-12-27 17:23:33 +0100

    added methods for debugging

---
+++ scripts/_debug_helpers.py
@@ -0,0 +1,20 @@
+'''Helpers for quick bug locating'''
+
+def remove_all_interconnectors(n):
+    '''helper function for debugging'''
+
+    for l in n.links.index[n.links.carrier == 'interconnector']:
+        n.remove("Link", l)
+    
+    for g in n.generators.index[n.generators.carrier == 'local_market']:
+        n.remove("Generator", g)
+    
+    for l in n.loads.index[n.loads.carrier != 'electricity']:
+        n.remove("Load", l)
+
+
+def remove_all_storages(n):
+    '''helper function for debugging'''
+
+    for s in n.storage_units.index:
+        n.remove("StorageUnit", s)
