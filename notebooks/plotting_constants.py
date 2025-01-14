6eddbd2fb0b589b8dd07e5c2e300302f794d41de
LukasFrankenQ <lukas.franken@ed.ac.uk>
2025-01-14 21:56:06 +0000

    added plotting of monthly results

---
+++ notebooks/plotting_constants.py
@@ -0,0 +1,17 @@
+nice_names = {
+    'wholesale': 'Wholesale Market',
+    'roc_payments': 'Renewable Obligation Certificates',
+    'cfd_payments': 'Contracts for Differences',
+    'congestion_rent': 'Intra-GB Congestion Rents',
+    'offer_cost': 'Redispatch - Offers',
+    'bid_cost': 'Redispatch - Bids',
+}
+
+color_dict = {
+    'wholesale': '#F78C6B',
+    'roc_payments': '#EF476F',
+    'cfd_payments': '#06D6A0',
+    'congestion_rent': '#FFD166',
+    'offer_cost': '#073B4C',
+    'bid_cost': '#118AB2',
+}
